dbbd298728f989eeef1d98abe03e7ee8e77e7074
Lucas <lucas_ccg@hotmail.com>
2023-06-30 20:51:20 -0300

    add get users by id controller spec

--- src/modules/users/get/byId/spec/get.users.by.id.controller.spec.ts
+++ src/modules/users/get/byId/spec/get.users.by.id.controller.spec.ts
@@ -1,59 +1,72 @@
-import { HttpException, NotFoundException } from '@nestjs/common';
-import { SearchMarvelHerosController } from '../search.marvel.heros.controller';
-import { SearchMarvelHerosUseCase } from '../search.marvel.heros.use.case';
-import { heroesDto } from '../../dto/heros.dtos';
-import { MarvelProvider } from 'src/modules/provider/marvel.provider';
-import { Cache } from 'cache-manager';
+import { NotFoundException } from '@nestjs/common';
+import { GetUsersByIdController } from '../get.users.by.id.controller';
+import { UsersService } from 'src/modules/users/services/users.service';
+import { GetUsersByIdUseCase } from '../get.users.by.id.use.case';
+import { PrismaService } from 'src/config/db/prisma.service';
+import { faker } from '@faker-js/faker';
 
-describe('SearchMarvelHerosController', () => {
-  let controller: SearchMarvelHerosController;
-  let useCase: SearchMarvelHerosUseCase;
-  let marvelProvider: MarvelProvider;
-  let cacheManager: Cache;
+describe('GetUsersByIdController', () => {
+  let controller: GetUsersByIdController;
+  let usersService: UsersService;
+  let useCase: GetUsersByIdUseCase;
+  let prismaService: PrismaService;
 
   beforeEach(() => {
-    marvelProvider = new MarvelProvider(cacheManager);
-    useCase = new SearchMarvelHerosUseCase(marvelProvider);
-    controller = new SearchMarvelHerosController(useCase);
+    prismaService = new PrismaService();
+    usersService = new UsersService(prismaService);
+    useCase = new GetUsersByIdUseCase(usersService);
+    controller = new GetUsersByIdController(useCase);
   });
-
-  it('should call the search use case and return the result', async () => {
-    const searchDTO = { name: 'Iron Man' };
-    const hero: heroesDto = { id: 1, name: 'Iron Man', description: '' };
-    jest.spyOn(useCase, 'execute').mockImplementation(async () => hero);
-
-    const result = await controller.create(searchDTO);
-
-    expect(useCase.execute).toHaveBeenCalledTimes(1);
-    expect(useCase.execute).toHaveBeenCalledWith(searchDTO.name);
-    expect(result).toBe(hero);
-  });
-
-  it('should throw a NotFoundException if the hero is not found', async () => {
-    const searchDTO = { name: 'Hulk' };
-    jest.spyOn(useCase, 'execute').mockImplementation(async () => {
-      throw new NotFoundException();
+  it('should return the user and its favorite heros', async () => {
+    const user = await prismaService.users.create({
+      data: {
+        username: faker.internet.userName(),
+        email: faker.internet.email(),
+        password: faker.internet.password(),
+      },
+    });
+    const hero1 = await prismaService.heros.create({
+      data: {
+        external_id: faker.number.int({ max: 10000 }),
+        name: faker.person.firstName(),
+        description: '',
+      },
+    });
+    const hero2 = await prismaService.heros.create({
+      data: {
+        external_id: faker.number.int({ max: 10000 }),
+        name: faker.person.firstName(),
+        description: '',
+      },
     });
 
-    await expect(controller.create(searchDTO)).rejects.toThrow(
-      NotFoundException,
-    );
-    expect(useCase.execute).toHaveBeenCalledTimes(1);
-    expect(useCase.execute).toHaveBeenCalledWith(searchDTO.name);
-  });
-
-  it('should throw an HttpException with the error message and status', async () => {
-    const searchDTO = { name: 'Iron Man' };
-    const errorMessage = 'Some error message';
-    const errorStatus = 500;
-    const error = new HttpException(errorMessage, errorStatus);
-    (error as any).status = errorStatus;
-    jest.spyOn(useCase, 'execute').mockImplementation(async () => {
-      throw new HttpException(errorMessage, errorStatus);
+    await prismaService.user_heros.create({
+      data: {
+        hero_id: hero1.external_id,
+        user_id: user.id,
+      },
+    });
+    await prismaService.user_heros.create({
+      data: {
+        hero_id: hero2.external_id,
+        user_id: user.id,
+      },
     });
 
-    await expect(controller.create(searchDTO)).rejects.toThrow(HttpException);
-    expect(useCase.execute).toHaveBeenCalledTimes(1);
-    expect(useCase.execute).toHaveBeenCalledWith(searchDTO.name);
+    const result = await controller.handle(user.id);
+
+    expect(result.user).toEqual({
+      id: user.id,
+      email: user.email,
+      username: user.username,
+    });
+    expect(result.favoriteHeros).toHaveLength(2);
+    expect(result.favoriteHeros).toEqual([hero1, hero2]);
+  });
+
+  it('should throw NotFoundException', async () => {
+    expect(async () => {
+      await controller.handle('1');
+    }).rejects.toThrowError(NotFoundException);
   });
 });
