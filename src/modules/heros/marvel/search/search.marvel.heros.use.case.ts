28d2587336ed76aea91f9a757386b85e6bdf56ba
Lucas <lucas_ccg@hotmail.com>
2023-06-30 19:31:28 -0300

    typing response

--- src/modules/heros/marvel/search/search.marvel.heros.use.case.ts
+++ src/modules/heros/marvel/search/search.marvel.heros.use.case.ts
@@ -1,11 +1,12 @@
 import { Injectable } from '@nestjs/common';
 import { MarvelProvider } from 'src/modules/provider/marvel.provider';
+import { heroesDto } from '../dto/heros.dtos';
 
 @Injectable()
 export class SearchMarvelHerosUseCase {
   constructor(private marvelProvider: MarvelProvider) {}
 
-  async execute(name: string) {
+  async execute(name: string): Promise<heroesDto | string> {
     return await this.marvelProvider.getHeroes(name);
   }
 }
